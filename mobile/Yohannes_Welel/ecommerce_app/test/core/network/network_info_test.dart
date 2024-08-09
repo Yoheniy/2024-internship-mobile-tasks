7c007b94d2a28db805815118f023d36434edde75
yohannes welel <125876217+Yoheniy@users.noreply.github.com>
2024-08-09 16:45:21 +0300

    "revise and refactor"

--- mobile/Yohannes_Welel/ecommerce_app/test/core/network/network_info_test.dart
+++ mobile/Yohannes_Welel/ecommerce_app/test/core/network/network_info_test.dart
@@ -1,40 +1,40 @@
-// import 'package:ecommerce_app/core/network/network_info.dart';
-// import 'package:flutter_test/flutter_test.dart';
-// import 'package:mockito/mockito.dart';
-// import '../../helpers/test_helper.mocks.dart';
+import 'package:ecommerce_app/core/network/network_info.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import '../../helpers/test_helper.mocks.dart';
 
 
-// void main() {
-//   late NetworkInfoImpl networkInfo;
-//   late MockInternetConnectionChecker mockInternetConnectionChecker;
+void main() {
+  late NetworkInfoImpl networkInfo;
+  late MockInternetConnectionChecker mockInternetConnectionChecker;
 
-//   setUp(() {
-//     mockInternetConnectionChecker = MockInternetConnectionChecker();
-//     networkInfo = NetworkInfoImpl(connectionChecker: mockInternetConnectionChecker);
-//   });
-
-
-//   group('isConnected', () {
-//     test(
-//       'should forward the call to DataConnectionChecker.hasConnection',
-//       () async {
-//         // arrange
+  setUp(() {
+    mockInternetConnectionChecker = MockInternetConnectionChecker();
+    networkInfo = NetworkInfoImpl(connectionChecker: mockInternetConnectionChecker);
+  });
 
 
-//         when(mockInternetConnectionChecker.hasConnection)
-//             .thenAnswer((_) async => true);
+  group('isConnected', () {
+    test(
+      'should forward the call to DataConnectionChecker.hasConnection',
+      () async {
+        // arrange
+
+
+        when(mockInternetConnectionChecker.hasConnection)
+            .thenAnswer((_) async => true);
 
      
-//         // act
-//         // NOTICE: We're NOT awaiting the result
-//         final result = await networkInfo.isConnected;
+        // act
+        // NOTICE: We're NOT awaiting the result
+        final result = await networkInfo.isConnected;
       
-//         // assert
-//         verify(mockInternetConnectionChecker.hasConnection);
-//         // Utilizing Dart's default referential equality.
-//         // Only references to the same object are equal.
-//         expect(result, true);
-//       },
-//     );
-//   });
-// }
+        // assert
+        verify(mockInternetConnectionChecker.hasConnection);
+        // Utilizing Dart's default referential equality.
+        // Only references to the same object are equal.
+        expect(result, true);
+      },
+    );
+  });
+}
